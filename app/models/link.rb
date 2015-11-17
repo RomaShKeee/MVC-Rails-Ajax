9ddfdef12d4ebd5d5d84aff2e831c22b74d7ea56
RomaShKeee <andreevroma@gmail.com>
2015-11-17 20:06:47 +0200

    validates url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ActiveRecord::Base
+  before_create :save_clear_url
+
+
+  VALID_URL_REGEX = /\A((http[s]?|ftp):\/)?\/?([^:\/\s]+)(\/|)\z/
+  validates :url, presence: true, format: { with: VALID_URL_REGEX }
+
+  private
+    def save_clear_url
+      path = VALID_URL_REGEX.match(self.url)
+      self.url = path[3].to_s.gsub('www.','')
+    end
 end
