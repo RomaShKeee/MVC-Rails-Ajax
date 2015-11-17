772929d39992a35633a33ea4ea619cced3641c69
RomaShKeee <andreevroma@gmail.com>
2015-11-17 19:01:03 +0200

    make ajax for form

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,35 @@
+class LinksController < ApplicationController
+  def index
+  end
+
+  def new
+    @link = LineItem.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to store_url }
+        format.js { @link }
+        format.json { render :show, status: :created, location: @link }
+      else
+        format.html { render :new }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to links_url, notice: 'Link item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def link_params
+      params.require(:link).permit(:url)
+  end
+
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def main
+    @link = Link.new
   end
 end
